7660885a5c669760be7e2b821ad0eb6be41d4a60
smaicas <samuel.maicas.development@gmail.com>
2023-04-02 17:51:47 +0200

    Init

---
+++ 1000/Nj.Train.Codeforces.MoveBrakets/Program.cs
@@ -0,0 +1,25 @@
+﻿int ncases = int.Parse(Console.ReadLine());
+
+List<dynamic> cases = new();
+for (int i = 1; i <= ncases; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+    string data = Console.ReadLine();
+    cases.Add(new { Num = num, Data = data });
+}
+
+foreach (dynamic @case in cases) Console.WriteLine(Res.Resolve(@case.Data));
+
+internal class Res
+{
+    public static int Resolve(string data)
+    {
+        while (data.Contains("()"))
+        {
+            data = data.Replace("()", "");
+            ;
+        }
+
+        return data.Length / 2;
+    }
+}
